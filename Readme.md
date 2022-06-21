be8926fa2893af507ecd8ba0cd9c2ac2e1ee596c
fikrifrramdani <fikriramdani32@gmail.com>
2022-06-21 23:27:45 +0700

    Push project ke github

---
+++ Readme.md
@@ -0,0 +1,3 @@
+#Blog Project With Django 1.11
+
+*Blog sederhana berbasis django 1.11 dengan menggunakan bahasa python
---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.views.generic import TemplateView
+from artikel.views import ArtikelPerKategori
+
+class BlogHomeView(TemplateView, ArtikelPerKategori):
+	template_name = "index.html"
+
+	def get_context_data(self):
+		querysets = self.get_latest_artikel_each_kategori()
+		context = {
+			'latest_artikel' : querysets,
+		}
+
+		return context
